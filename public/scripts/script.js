87b9b2e289090c5c563f9a9c18d65c6dbf44e759
Taylor Farley <twfarley88@gmail.com>
2020-09-07 10:07:48 -0400

    adding front end

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,15 +5,16 @@ document.querySelector('#buttonMe').addEventListener('click', () => {
     fetch(`/getVirus`)
     .then((response) => response.json())
     .then((data) => {
-        const x = data.data.length
+  
         covidData = data.data;
         displayData = covidData.filter(data => data["Province"] === 'Ontario');
         
         const n1 = (displayData[displayData.length-1].Cases - displayData[displayData.length-2].Cases)
-        // console.log(displayData[displayData.length-1].Cases)
-        // console.log(n1)
-        
-        console.log(displayData[displayData.length-1].Date + ' Count is : ' + n1)       
+
+        const Displaydate = moment(displayData.Date).format("YYYY-MM-DD")
+ 
+        console.log(Displaydate + ' Count is : ' + n1)       
+        console.log(Displaydate)
     })      
 });
 
